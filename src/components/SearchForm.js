d25df82212d8b85c0fed2268744ff66edb0bcca2
trodriguez89 <trodriguez89@gmail.com>
2019-11-15 08:53:52 -0800

    search form wip

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,8 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 export default function SearchForm(props) {
- 
+ const [searchTerm, setSearchTerm] = useState("")
+ const [searchResults, setSearchResults] = useState([])
 
+ useEffect(() => {
+  const results = props.characterArray.filter(character => character.toLowerCase().includes(searchTerm.toLowerCase())
+  )
+  setSearchResults(results)
+ },[searchTerm])
+
+const handleChange = event => {
+  setSearchTerm(event.target.value);
+}
 
   return (
     <section className="search-form">
@@ -13,8 +23,10 @@ export default function SearchForm(props) {
       type="text"
       name="textfield"
       placeholder="Search"
-      
+      onChange={handleChange}
+      value={searchTerm}
       />
+      
 
      </form>
     </section>
